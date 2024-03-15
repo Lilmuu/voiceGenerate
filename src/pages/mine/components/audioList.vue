ed5c515424c6794b8aad40c0db6d6b0611efa08e
wjfe <1374144742@qq.com>
2024-03-15 11:42:44 +0800

    feat: 新增权限页面和音频播放页面完成

--- src/pages/mine/components/audioList.vue
+++ src/pages/mine/components/audioList.vue
@@ -1,13 +1,21 @@
 <script lang="ts" setup>
-import { ref, Ref } from 'vue'
+import { ref, Ref, unref } from 'vue'
 import { onMounted } from 'vue'
 import { mineApi } from '@/api/'
-import { audioList } from '../type/index'
+import { audioList, audioItem } from '../type/index'
 import { AudioControl } from '@/utils/audio'
+import VoiceList from '@/components/voiceList/index.vue'
+import { HandleAudioParam } from '@/components/voiceList/types/index'
 
 const audioList: Ref<Array<audioList>> = ref([])
 
 const total = ref(0)
+
+const openCollapse = ref(0)
+
+const changeCollapse = (e: number) => {
+  openCollapse.value = e
+}
 
 const pageData = ref({
   page: 1,
@@ -16,7 +24,21 @@ const pageData = ref({
 
 const queryAudioListAPI: () => Promise<void> = async () => {
   const res = await mineApi.queryAudioList(pageData.value)
-  audioList.value = res.data
+  audioList.value = res.data.length
+    ? res.data.map((list: any) => {
+        list.data = list.data.map((item: audioItem) => {
+          return {
+            auditionSound: item.audioUrl,
+            soundColorId: item.id,
+            soundColorInfo: item.text,
+            soundColorName: item.role,
+            audioStatus: 'play'
+          }
+        })
+        return list
+      })
+    : []
+  console.log(audioList.value)
 }
 
 // 下载音频
@@ -29,6 +51,12 @@ const downLoadFile = (url: string) => {
       }
     }
   })
+}
+
+const handleAudioList = ({ activeIndex, audioStatus }: HandleAudioParam) => {
+  const index = unref(activeIndex)
+  audioList.value[openCollapse.value].data[activeIndex.value].audioStatus =
+    audioStatus
 }
 
 onMounted(() => {
@@ -48,38 +76,24 @@ onMounted(() => {
     >
     </u-navbar>
     <div class="content-box pl pr">
-      <u-collapse accordion>
+      <u-collapse @open="changeCollapse" :value="openCollapse" accordion>
         <u-collapse-item
-          v-for="item in audioList"
+          v-for="(item, index) in audioList"
           :key="item.id"
           :title="item.title"
-          :name="item.title"
+          :name="index"
         >
-          <div class="audio-list">
-            <div v-for="child in item.data" :key="child.id" class="audio-item">
-              <div class="intro-box">
-                <div class="title">{{ child.role }}</div>
-                <up-text :lines="2" :text="child.text"></up-text>
-              </div>
-              <div class="btn-box">
-                <up-button
-                  size="small"
-                  class="mr-2"
-                  type="success"
-                  shape="circle"
-                  @click="AudioControl.addChooseAudio(child.id, child.audioUrl)"
-                  text="试听"
-                ></up-button>
-                <up-button
-                  size="small"
-                  shape="circle"
-                  type="primary"
-                  text="下载"
-                  @click="downLoadFile(child.audioUrl)"
-                ></up-button>
-              </div>
-            </div>
-          </div>
+          <VoiceList :voiceData="item.data" @handleAudioList="handleAudioList">
+            <template #btn="{ voiceItemData }">
+              <up-button
+                size="small"
+                shape="circle"
+                type="primary"
+                text="下载"
+                @click="downLoadFile(voiceItemData.auditionSound)"
+              ></up-button>
+            </template>
+          </VoiceList>
         </u-collapse-item>
       </u-collapse>
     </div>
--- src/pages/mine/type/index.ts
+++ src/pages/mine/type/index.ts
@@ -1,14 +1,18 @@
+type AudioStauts = 'play' | 'pause'
+import { VoiceData } from '@/components/voiceList/types/index'
+
 export interface audioItem {
   audioUrl: string
   id: number
   role: string
   text: string
+  audioStatus: AudioStauts
 }
 
 export interface audioList {
   id: number
   title: string
-  data: Array<audioItem>
+  data: Array<VoiceData>
 }
 // 单个消耗记录
 export interface recordItem {
